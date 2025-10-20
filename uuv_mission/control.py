4f3996ffc7685356b126e9b5dcd378895e461b59
Abel-Johan <nehemiahabelcjohan@gmail.com>
2025-10-20 10:37:04 +0100

    Implemented controller in control.py

---
+++ uuv_mission/control.py
@@ -0,0 +1,9 @@
+# PD Controller for depth regulation of an underwater vehicle
+# Mathematically, u[t] = Kp * e[t] + Kd * (e[t] - e[t-1])
+def controller(reference: float, depth: float, reference_prev: float, depth_prev: float, Kp: float, Kd: float) -> float:
+    error = reference - depth
+    error_prev = reference_prev - depth_prev
+    derivative = error - error_prev
+    control_signal = Kp * error + Kd * derivative
+    return control_signal
+    
